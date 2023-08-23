73f873921edccac60d477ecbe6dae54ea948a1e3
devmisterio <arslan_semih@yahoo.com>
2023-08-23 10:26:14 +0300

    added yargs for parsing cli arguments for animal

--- src/bin/bullspeak.ts
+++ src/bin/bullspeak.ts
@@ -1,6 +1,8 @@
 import * as readline from "readline";
+import * as yargs from "yargs";
+
 import { BalloonBuilder } from "../lib/class/BalloonBuilder";
-import { Animal } from "../lib/interface/Animal";
+
 import { Cat } from "../lib/class/animals/Cat";
 import { Fish } from "../lib/class/animals/Fish";
 import { Bull } from "../lib/class/animals/Bull";
@@ -12,6 +14,23 @@ async function main() {
     console.log("Usage: fortune | bullspeak");
     return;
   }
+
+  const argv = yargs
+    .scriptName("bullspeak")
+    .usage("Usage: <cmd> | $0 -a animal")
+    .example("$0 -a cat", "Show a cat speaking")
+    .option("a", {
+      alias: "animal",
+      describe: "Animal to show",
+      type: "string",
+      choices: ["cat", "fish", "bull"],
+      default: "bull",
+    })
+    .describe("help", "Show help")
+    .describe("version", "Show version number.")
+    .epilog("copyright 2023").argv;
+
+  console.log(argv);
 
   const lines: string[] = [];
   const rl = readline.createInterface({
